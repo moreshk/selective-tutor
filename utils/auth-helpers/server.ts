f1798a98dd993faf7ddbf9736cb7a7f576ab6009
moreshk <moreshk@gmail.com>
2024-09-14 17:39:15 +1000

    go to account page on log in

--- utils/auth-helpers/server.ts
+++ utils/auth-helpers/server.ts
@@ -151,7 +151,8 @@ export async function signInWithPassword(formData: FormData) {
     );
   } else if (data.user) {
     cookieStore.set('preferredSignInView', 'password_signin', { path: '/' });
-    redirectPath = getStatusRedirect('/', 'Success!', 'You are now signed in.');
+    // redirectPath = getStatusRedirect('/', 'Success!', 'You are now signed in.');
+    redirectPath = getStatusRedirect('/account', 'Success!', 'You are now signed in.');
   } else {
     redirectPath = getErrorRedirect(
       '/signin/password_signin',
